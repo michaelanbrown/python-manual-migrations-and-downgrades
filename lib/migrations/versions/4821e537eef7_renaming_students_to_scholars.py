e3fa61c467bad13749694eec24f37aaec19341f1
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-31 16:08:10 -0400

    generating migration

---
+++ lib/migrations/versions/4821e537eef7_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 4821e537eef7
+Revises: 791279dd0760
+Create Date: 2024-10-31 16:07:58.713342
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4821e537eef7'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
