8be8329a7224e1d8b8ef541e7206ca0b6a020ae3
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-31 16:17:42 -0400

    changing birthday to birth_date

---
+++ lib/migrations/versions/ff0daeaa6949_renaming_birthday_to_birth_date.py
@@ -0,0 +1,24 @@
+"""Renaming birthday to birth_date
+
+Revision ID: ff0daeaa6949
+Revises: 4821e537eef7
+Create Date: 2024-10-31 16:17:22.492099
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ff0daeaa6949'
+down_revision = '4821e537eef7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
